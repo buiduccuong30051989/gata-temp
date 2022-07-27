0467b4275b6c43f1195e3c8729857720ae87b7b0
Cuong <bui.duc.cuong@sun-asterisk.com>
2022-07-27 22:18:52 +0700

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ module.exports = {
         100: "#D4EEE2",
         50: "#F1FAF5",
         "gradient-400": "linear-gradient(180deg, #23C277 0%, #399D6C 100%)",
-        "gradient-500": "linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(180deg, #23C277 0%, #399D6C 100%)",
+        "gradient-500":
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(180deg, #23C277 0%, #399D6C 100%)",
         "gradient-900": "linear-gradient(180deg, #208A58 1.91%, #329966 100%)",
       },
       info: {
@@ -31,8 +32,9 @@ module.exports = {
         400: "#1070CA",
         100: "#084B8A",
         50: "#F6F9FD",
-        "gradient-400": "inear-gradient(360deg, #116AB8 0%, #0788DE 100%)",
-        "gradient-500": "llinear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(360deg, #116AB8 0%, #0788DE 100%)",
+        "gradient-400": "linear-gradient(360deg, #116AB8 0%, #0788DE 100%)",
+        "gradient-500":
+          "llinear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(360deg, #116AB8 0%, #0788DE 100%)",
         "gradient-900": "linear-gradient(360deg, #1663A7 0.22%, #0F5A8C 100%)",
       },
       warning: {
@@ -42,7 +44,8 @@ module.exports = {
         100: "#FAE3CD",
         50: "#FDF8F3",
         "gradient-400": "linear-gradient(180deg, #EE9913 0%, #D9822B 100%)",
-        "gradient-500": "linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(180deg, #EE9913 0%, #D9822B 100%)",
+        "gradient-500":
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(180deg, #EE9913 0%, #D9822B 100%)",
         "gradient-900": "linear-gradient(180deg, #A76A0B 0.78%, #C4701C 100%)",
       },
       danger: {
@@ -52,7 +55,8 @@ module.exports = {
         100: "#FAE2E2",
         50: "#FEF6F6",
         "gradient-400": "linear-gradient(180deg, #EE5029 0%, #EC4C47 100%)",
-        "gradient-500": "linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(180deg, #F16F4F 0%, #EC4C47 100%)",
+        "gradient-500":
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), linear-gradient(180deg, #F16F4F 0%, #EC4C47 100%)",
         "gradient-900": "linear-gradient(180deg, #EC4C47 0%, #D64540 97.57%)",
       },
       yellow: {
@@ -86,8 +90,15 @@ module.exports = {
         100: "#F5F6F7", // lighter
         50: "#FAFAFA", // lightest
       },
-      aqua: { 
+      aqua: {
         50: "rgba(16, 112, 202, 0.08)",
+      },
+      light: {
+        100: "rgba(67, 90, 111, 0.06)",
+        "gradient-100":
+          "linear-gradient(360deg, #F4F5F7 0%, #FFFFFF 100%), #FFFFFF",
+        "gradient-200":
+          "linear-gradient(360deg, #EAECEE 0%, #FAFBFB 100%), #FFFFFF",
       },
     },
     extend: {
@@ -102,16 +113,15 @@ module.exports = {
         400: "0px 0px 1px rgba(67, 90, 111, 0.47)",
         500: "0px 0px 1px rgba(67, 90, 111, 0.47)",
         "inset-100": "inset 0px 1px 1px rgba(67, 90, 111, 0.09)", // active regular button
-        "inset-200": "inset 0px 1px 2px rgba(67, 90, 111, 0.2)", // form 
+        "inset-200": "inset 0px 1px 2px rgba(67, 90, 111, 0.2)", // form
         "inset-1": "inset 0px -1px 1px rgba(67, 90, 111, 0.2)", // default button
         "inset-2": "inset 0px -1px 1px rgba(67, 90, 111, 0.09)", // focus/hover button
         "inset-3": "inset 0px 1px 1px rgba(67, 90, 111, 0.2)", // active button
       },
       borderRadius: {
-        DEFAULT: '3px',
+        DEFAULT: "3px",
       },
     },
   },
   plugins: [],
 };
- 
